dcc1c246299b9ff69558549c4c72e5f4e92a13a3
khirodh kumar choudhary <111672869+khirodh-kumar-choudhary@users.noreply.github.com>
2022-08-21 18:10:30 +0530

    python source

---
+++ 50 python codes/48 -- program to print all the items in a dictionary-checkpoint.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "35eb5aba",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "phone_book = {\n",
+    "'khirodh' : [ '9381078783', 'kkc@abcmail.com' ],\n",
+    "'Bob': [ '7994880000', 'bob@xyzmail.com' ],\n",
+    "'Tom' : [ '9749552647' , 'tom@xyzmail.com' ]\n",
+    "}\n",
+    "for k,v in phone_book.items():\n",
+    "    print(k, \":\", v)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
